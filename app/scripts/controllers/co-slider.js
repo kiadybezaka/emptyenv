244a1ed40badbc6d8d0a46f50710ee12b46356f2
Kiady Bezaka <kiady.razafindrabe@gmail.com>
2017-06-06 07:51:59 +0300

    Slide content & Footer

--- app/scripts/controllers/co-slider.js
+++ app/scripts/controllers/co-slider.js
@@ -7,7 +7,7 @@ module.exports = function () {
 
   'use strict';
 
-
+  
   ////////////////////////////////////////////
   //----- carousel for homepage banner ----//
   //////////////////////////////////////////
@@ -25,5 +25,42 @@ module.exports = function () {
       center: true,
       infinite: true,
       arrows: false
+    });
+    //////////////////////////////////////
+  //----- carousel for road slider ----//
+  //////////////////////////////////////
+    var roadSlider = '.js-slide-road';
+    //initialize slick.js
+    $(roadSlider).slick({
+      rows: 1,
+      adaptiveHeight: true,
+      slidesToShow: 9,
+      slidesToScroll: 9,
+      dots: false,
+      infinite: false,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 1600,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 6,
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
     });
 };
