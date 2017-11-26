7939f7eba97c1222a354a3d57bc39781cc8c1fdb
Kiady Bezaka <kiady.razafindrabe@gmail.com>
2017-11-26 23:01:40 +0300

    [section] contact & media

--- app/scripts/controllers/co-test.js
+++ app/scripts/controllers/co-test.js
@@ -1,11 +1,13 @@
 var _jsTest = require('./../classes/cl-test');
-
+var slick = require('slick-carousel');
+var simpleParallax = require('simple-parallax-jquery');
 module.exports = function() {
 
     'use strict';
 
-    $(_jsTest.jsTestBtn).on('click', function(e) {
-        e.preventDefault();
-        _jsTest.testWsFunc();
-    })
+        $('.js-parallax').simpleParallax({ scale: '1.30', orientation: 'up' });
+        $(_jsTest.jsTestBtn).on('click', function(e) {
+                e.preventDefault();
+                _jsTest.testWsFunc();
+        })
 }
