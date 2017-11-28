f0879b915db66421294be1d3bb9d2621b4a22b98
Kiady Bezaka <kiady.razafindrabe@gmail.com>
2017-11-28 06:46:18 +0300

    [section] anchor

--- app/scripts/controllers/co-test.js
+++ app/scripts/controllers/co-test.js
@@ -10,4 +10,11 @@ module.exports = function() {
                 e.preventDefault();
                 _jsTest.testWsFunc();
         })
+        $(document).on('click', 'a[href^="#"]', function (event) {
+                event.preventDefault();
+
+                $('html, body').animate({
+                        scrollTop: $($.attr(this, 'href')).offset().top - 150
+                }, 500);
+        });
 }
