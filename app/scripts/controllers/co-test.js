32467a4c6588d4c6d9d8813c1d77edf269d1fa0f
krazafin <razafindrabeb@proximity.bbdo.fr>
2017-12-12 19:26:57 +0400

    [section] Album - Lightbox

--- app/scripts/controllers/co-test.js
+++ app/scripts/controllers/co-test.js
@@ -1,20 +1,24 @@
 var _jsTest = require('./../classes/cl-test');
 var slick = require('slick-carousel');
 var simpleParallax = require('simple-parallax-jquery');
+var lightbox = require('lightbox2');
 module.exports = function() {
 
-    'use strict';
+  'use strict';
 
-        $('.js-parallax').simpleParallax({ scale: '1.30', orientation: 'up' });
-        $(_jsTest.jsTestBtn).on('click', function(e) {
-                e.preventDefault();
-                _jsTest.testWsFunc();
-        })
-        $(document).on('click', 'a[href^="#"]', function (event) {
-                event.preventDefault();
-
-                $('html, body').animate({
-                        scrollTop: $($.attr(this, 'href')).offset().top - 150
-                }, 800);
-        });
+  $('.js-parallax').simpleParallax({ scale: '1.30', orientation: 'up' });
+  $(_jsTest.jsTestBtn).on('click', function(e) {
+    e.preventDefault();
+    _jsTest.testWsFunc();
+  })
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 150
+    }, 800);
+  });
+  lightbox.option({
+    showImageNumberLabel: true,
+    albumLabel:	"Image %1 sur %2"
+  })
 }
